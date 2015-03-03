5c10c91a1ef0c9f6297101925e5487ff329a9cda
Yorick Peterse <yorickpeterse@gmail.com>
2015-03-03 21:02:34 +0100

    Specs for Addrinfo.foreach

--- spec/addrinfo/foreach_spec.rb
+++ spec/addrinfo/foreach_spec.rb
@@ -1,5 +1,16 @@
 require 'socket'
 
-describe "Addrinfo.foreach" do
-  it "needs to be reviewed for spec completeness"
+describe 'Addrinfo.foreach' do
+  it 'yields Addrinfo instances  to the supplied block' do
+    yielded = []
+
+    Addrinfo.foreach('localhost', 80) do |addr|
+      addr.should be_an_instance_of(Addrinfo)
+
+      yielded << addr
+    end
+
+    # To check if the block is actually called.
+    yielded.empty?.should == false
+  end
 end
