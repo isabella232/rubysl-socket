91bb4c4bc62aa45ac4a541cfa280d283af3b926b
Yorick Peterse <yorickpeterse@gmail.com>
2015-03-03 21:02:25 +0100

    Specs for Addrinfo#afamily

--- spec/addrinfo/afamily_spec.rb
+++ spec/addrinfo/afamily_spec.rb
@@ -1,5 +1,15 @@
 require 'socket'
 
-describe "Addrinfo#afamily" do
-  it "needs to be reviewed for spec completeness"
+describe 'Addrinfo#afamily' do
+  it 'returns AF_INET as the default address family' do
+    sockaddr = Socket.pack_sockaddr_in(80, 'localhost')
+
+    Addrinfo.new(sockaddr).afamily.should == Socket::AF_INET
+  end
+
+  it 'returns AF_UNIX as the address family for Unix sockets' do
+    sockaddr = Socket.pack_sockaddr_un('socket')
+
+    Addrinfo.new(sockaddr).afamily.should == Socket::AF_UNIX
+  end
 end
