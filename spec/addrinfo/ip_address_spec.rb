7ef2ce5dabf3c78d98cfe2c0978d7f8f8f1af461
Yorick Peterse <yorickpeterse@gmail.com>
2015-03-03 21:02:55 +0100

    Specs for Addrinfo#ip_address

--- spec/addrinfo/ip_address_spec.rb
+++ spec/addrinfo/ip_address_spec.rb
@@ -1,5 +1,21 @@
 require 'socket'
 
-describe "Addrinfo#ip_address" do
-  it "needs to be reviewed for spec completeness"
+describe 'Addrinfo#ip_address' do
+  describe 'using a String as the socket address' do
+    it 'returns the IP as a String' do
+      sockaddr = Socket.sockaddr_in(80, '127.0.0.1')
+      addr     = Addrinfo.new(sockaddr)
+
+      addr.ip_address.should == '127.0.0.1'
+    end
+  end
+
+  describe 'using an Array as the socket address' do
+    it 'returns the IP as a String' do
+      sockaddr = ['AF_INET', 80, 'localhost', '127.0.0.1']
+      addr     = Addrinfo.new(sockaddr)
+
+      addr.ip_address.should == '127.0.0.1'
+    end
+  end
 end
