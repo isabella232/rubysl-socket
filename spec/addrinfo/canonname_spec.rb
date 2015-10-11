9e6f9435a6c7d1ba5ea6e45801f9fb639a3a150a
Yorick Peterse <yorickpeterse@gmail.com>
2015-10-11 21:28:31 +0200

    Added spec boilerplate for Addrinfo#canonname

--- spec/addrinfo/canonname_spec.rb
+++ spec/addrinfo/canonname_spec.rb
@@ -1,5 +1,19 @@
 require 'socket'
 
-describe "Addrinfo#canonname" do
-  it "needs to be reviewed for spec completeness"
+describe 'Addrinfo#canonname' do
+  describe 'when the canonical name is available' do
+    it 'returns the canonical name' do
+      # TODO: figure out how we're going to set the canonical name
+    end
+  end
+
+  describe 'when the canonical name is not available' do
+    before do
+      @addr = Addrinfo.new('127.0.0.1', 9999)
+    end
+
+    it 'returns nil' do
+      @addr.canonname.should be_nil
+    end
+  end
 end
