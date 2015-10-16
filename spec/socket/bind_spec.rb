50b5310b4334aedede46ee35839aea93d4555f97
Yorick Peterse <yorickpeterse@gmail.com>
2015-10-16 15:47:25 +0200

    Cleaned up Socket#bind specs
    Lets not pollute the global scope with Socket::Constants just because
    we're too lazy to type "Socket::AF_INET" instead of "AF_INET".

--- spec/socket/bind_spec.rb
+++ spec/socket/bind_spec.rb
@@ -1,14 +1,12 @@
 require File.expand_path('../../fixtures/classes', __FILE__)
 
-include Socket::Constants
-
 describe "Socket#bind on SOCK_DGRAM socket" do
-  before :each do
-    @sock = Socket.new(AF_INET, SOCK_DGRAM, 0);
+  before do
+    @sock = Socket.new(Socket::AF_INET, Socket::SOCK_DGRAM, 0);
     @sockaddr = Socket.pack_sockaddr_in(SocketSpecs.port, "127.0.0.1");
   end
 
-  after :each do
+  after do
     @sock.closed?.should be_false
     @sock.close
   end
@@ -29,25 +27,28 @@ describe "Socket#bind on SOCK_DGRAM socket" do
 
   it "raises Errno::EADDRNOTAVAIL when the specified sockaddr is not available from the local machine" do
     sockaddr1 = Socket.pack_sockaddr_in(SocketSpecs.port, "4.3.2.1");
+
     lambda { @sock.bind(sockaddr1); }.should raise_error(Errno::EADDRNOTAVAIL)
   end
 
   platform_is_not :os => [:windows, :cygwin] do
     it "raises Errno::EACCES when the current user does not have permission to bind" do
       sockaddr1 = Socket.pack_sockaddr_in(1, "127.0.0.1");
+
       lambda { @sock.bind(sockaddr1); }.should raise_error(Errno::EACCES)
     end
   end
 end
 
 describe "Socket#bind on SOCK_STREAM socket" do
-  before :each do
-    @sock = Socket.new(AF_INET, SOCK_STREAM, 0);
-    @sock.setsockopt(SOL_SOCKET, SO_REUSEADDR, true)
+  before do
+    @sock = Socket.new(Socket::AF_INET, Socket::SOCK_STREAM, 0);
+    @sock.setsockopt(Socket::SOL_SOCKET, Socket::SO_REUSEADDR, true)
+
     @sockaddr = Socket.pack_sockaddr_in(SocketSpecs.port, "127.0.0.1");
   end
 
-  after :each do
+  after do
     @sock.closed?.should be_false
     @sock.close
   end
@@ -68,12 +69,14 @@ describe "Socket#bind on SOCK_STREAM socket" do
 
   it "raises Errno::EADDRNOTAVAIL when the specified sockaddr is not available from the local machine" do
     sockaddr1 = Socket.pack_sockaddr_in(SocketSpecs.port, "4.3.2.1");
+
     lambda { @sock.bind(sockaddr1); }.should raise_error(Errno::EADDRNOTAVAIL)
   end
 
   platform_is_not :os => [:windows, :cygwin] do
     it "raises Errno::EACCES when the current user does not have permission to bind" do
       sockaddr1 = Socket.pack_sockaddr_in(1, "127.0.0.1");
+
       lambda { @sock.bind(sockaddr1); }.should raise_error(Errno::EACCES)
     end
   end
