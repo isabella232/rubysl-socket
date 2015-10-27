1fab6e87064e34faae812243c841812e0de299f5
Yorick Peterse <yorickpeterse@gmail.com>
2015-10-27 22:04:24 +0100

    Socket#accept_nonblock spec for blocking callers

--- spec/socket/accept_nonblock_spec.rb
+++ spec/socket/accept_nonblock_spec.rb
@@ -31,47 +31,61 @@ describe 'Socket#accept_nonblock' do
     before do
       @server.bind(@sockaddr)
       @server.listen(1)
-
-      @client = Socket.new(:INET, :STREAM, 0)
-      addr    = Socket.sockaddr_in(@server.local_address.ip_port, '127.0.0.1')
-
-      @client.connect(addr)
     end
 
-    it 'returns an Array containing a Socket and an Addrinfo' do
-      socket, addrinfo = @server.accept_nonblock
-
-      socket.should be_an_instance_of(Socket)
-      addrinfo.should be_an_instance_of(Addrinfo)
+    describe 'without a connected client' do
+      it 'raises IO::EAGAINWaitReadable' do
+        proc { @server.accept_nonblock }.should raise_error(IO::EAGAINWaitReadable)
+      end
     end
 
-    describe 'the returned Addrinfo' do
+    describe 'with a connected client' do
       before do
-        _, @addr = @server.accept_nonblock
+        addr    = Socket.sockaddr_in(@server.local_address.ip_port, '127.0.0.1')
+        @client = Socket.new(:INET, :STREAM, 0)
+
+        @client.connect(addr)
       end
 
-      it 'uses AF_INET as the address family' do
-        @addr.afamily.should == Socket::AF_INET
+      after do
+        @client.close
       end
 
-      it 'uses PF_INET as the protocol family' do
-        @addr.pfamily.should == Socket::PF_INET
+      it 'returns an Array containing a Socket and an Addrinfo' do
+        socket, addrinfo = @server.accept_nonblock
+
+        socket.should be_an_instance_of(Socket)
+        addrinfo.should be_an_instance_of(Addrinfo)
       end
 
-      it 'uses SOCK_STREAM as the socket type' do
-        @addr.socktype.should == Socket::SOCK_STREAM
-      end
+      describe 'the returned Addrinfo' do
+        before do
+          _, @addr = @server.accept_nonblock
+        end
 
-      it 'uses 0 as the protocol' do
-        @addr.protocol.should == 0
-      end
+        it 'uses AF_INET as the address family' do
+          @addr.afamily.should == Socket::AF_INET
+        end
 
-      it 'uses the same IP address as the client Socket' do
-        @addr.ip_address.should == @client.local_address.ip_address
-      end
+        it 'uses PF_INET as the protocol family' do
+          @addr.pfamily.should == Socket::PF_INET
+        end
 
-      it 'uses the same port as the client Socket' do
-        @addr.ip_port.should == @client.local_address.ip_port
+        it 'uses SOCK_STREAM as the socket type' do
+          @addr.socktype.should == Socket::SOCK_STREAM
+        end
+
+        it 'uses 0 as the protocol' do
+          @addr.protocol.should == 0
+        end
+
+        it 'uses the same IP address as the client Socket' do
+          @addr.ip_address.should == @client.local_address.ip_address
+        end
+
+        it 'uses the same port as the client Socket' do
+          @addr.ip_port.should == @client.local_address.ip_port
+        end
       end
     end
   end
