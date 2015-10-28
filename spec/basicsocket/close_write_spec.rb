6c374116d2c498b59be850f0ccd44b589bd39680
Yorick Peterse <yorickpeterse@gmail.com>
2015-10-28 17:54:52 +0100

    Cleaned up BasicSocket#close_write specs

--- spec/basicsocket/close_write_spec.rb
+++ spec/basicsocket/close_write_spec.rb
@@ -1,42 +1,43 @@
-require File.expand_path('../../fixtures/classes', __FILE__)
+require 'socket'
 
-describe "BasicSocket#close_write" do
-  before :each do
-    @server = TCPServer.new(SocketSpecs.port)
+describe 'BasicSocket#close_write' do
+  before do
+    @server = Socket.new(:INET, :STREAM, 0)
   end
 
-  after :each do
-    @server.close unless @server.closed?
-  end
-
-  it "closes the writing end of the socket" do
+  it 'closes the socket for write operations' do
     @server.close_write
-    lambda { @server.write("foo") }.should raise_error(IOError)
+
+    proc { @server.write('foo') }.should raise_error(IOError)
   end
 
-  it "works on sockets with closed write ends" do
+  it 'does not raise when called on a socket already closed for writing' do
     @server.close_write
-    lambda { @server.close_write }.should_not raise_error(Exception)
-    lambda { @server.write("foo") }.should raise_error(IOError)
+
+    proc { @server.close_write }.should_not raise_error
+    proc { @server.write('foo') }.should raise_error(IOError)
   end
 
-  it "does not close the socket" do
+  it 'does not fully close the socket' do
     @server.close_write
     @server.closed?.should be_false
   end
 
-  it "fully closes the socket if it was already closed for reading" do
+  it 'fully closes the socket if it was already closed for reading' do
     @server.close_read
+    @server.closed?.should be_false
+
     @server.close_write
     @server.closed?.should be_true
   end
 
-  it "raises IOError on closed socket" do
+  it 'raises IOError when called on a fully closed socket' do
     @server.close
-    lambda { @server.close_write }.should raise_error(IOError)
+
+    proc { @server.close_write }.should raise_error(IOError)
   end
 
-  it "returns nil" do
+  it 'returns nil' do
     @server.close_write.should be_nil
   end
 end
