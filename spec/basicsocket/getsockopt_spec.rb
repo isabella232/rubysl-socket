bf4892ec1966ddba55eb51a04ae0bc750253057e
Yorick Peterse <yorickpeterse@gmail.com>
2015-10-30 21:05:14 +0100

    Refactored BasicSocket#getsockopt specs

--- spec/basicsocket/getsockopt_spec.rb
+++ spec/basicsocket/getsockopt_spec.rb
@@ -1,54 +1,166 @@
-require File.expand_path('../../fixtures/classes', __FILE__)
+require 'socket'
 
-describe "BasicSocket#getsockopt" do
-  before(:each) do
-    @sock = Socket.new(Socket::AF_INET, Socket::SOCK_STREAM, 0)
+describe 'BasicSocket#getsockopt' do
+  before do
+    @sock = Socket.new(:INET, :STREAM)
   end
 
-  after :each do
-    @sock.closed?.should be_false
-    @sock.close
+  it 'returns a Socket::Option using a constant' do
+    opt = @sock.getsockopt(Socket::SOL_SOCKET, Socket::SO_TYPE)
+
+    opt.should be_an_instance_of(Socket::Option)
   end
 
-  it "gets a socket option Socket::SO_TYPE" do
-    n = @sock.getsockopt(Socket::SOL_SOCKET, Socket::SO_TYPE).to_s
-    n.should == [Socket::SOCK_STREAM].pack("i")
+  it 'returns a Socket::Option for a boolean option' do
+    opt = @sock.getsockopt(Socket::SOL_SOCKET, Socket::SO_REUSEADDR)
+
+    opt.bool.should == false
   end
 
-  it "gets a socket option Socket::SO_OOBINLINE" do
-    n = @sock.getsockopt(Socket::SOL_SOCKET, Socket::SO_OOBINLINE).to_s
-    n.should == [0].pack("i")
+  it 'returns a Socket::Option for a numeric option' do
+    opt = @sock.getsockopt(Socket::IPPROTO_IP, Socket::IP_TTL)
+
+    opt.int.should be_an_instance_of(Fixnum)
   end
 
-  it "gets a socket option Socket::SO_LINGER" do
-    n = @sock.getsockopt(Socket::SOL_SOCKET, Socket::SO_LINGER).to_s
-    if (n.size == 8) # linger struct on some platforms, not just a value
-      n.should == [0, 0].pack("ii")
-    else
-      n.should == [0].pack("i")
+  it 'returns a Socket::Option for a struct option' do
+    opt = @sock.getsockopt(Socket::SOL_SOCKET, Socket::SO_LINGER)
+
+    opt.linger.should == [false, 0]
+  end
+
+  it 'raises Errno::ENOPROTOOPT when requesting an invalid option' do
+    proc { @sock.getsockopt(Socket::SOL_SOCKET, -1) }
+      .should raise_error(Errno::ENOPROTOOPT)
+  end
+
+  describe 'using Symbols as arguments' do
+    it 'returns a Socket::Option for arguments :SOCKET and :TYPE' do
+      opt = @sock.getsockopt(:SOCKET, :TYPE)
+
+      opt.level.should   == Socket::SOL_SOCKET
+      opt.optname.should == Socket::SO_TYPE
+    end
+
+    it 'returns a Socket::Option for arguments :IP and :TTL' do
+      opt = @sock.getsockopt(:IP, :TTL)
+
+      opt.level.should   == Socket::SOL_IP
+      opt.optname.should == Socket::IP_TTL
+    end
+
+    it 'returns a Socket::Option for arguments :IP and :MULTICAST_TTL' do
+      opt = @sock.getsockopt(:IP, :MULTICAST_TTL)
+
+      opt.level.should   == Socket::SOL_IP
+      opt.optname.should == Socket::IP_MULTICAST_TTL
+    end
+
+    it 'returns a Socket::Option for arguments :SOCKET and :REUSEADDR' do
+      opt = @sock.getsockopt(:SOCKET, :REUSEADDR)
+
+      opt.level.should   == Socket::SOL_SOCKET
+      opt.optname.should == Socket::SO_REUSEADDR
+    end
+
+    it 'returns a Socket::Option for arguments :SOCKET and :LINGER' do
+      opt = @sock.getsockopt(:SOCKET, :LINGER)
+
+      opt.level.should   == Socket::SOL_SOCKET
+      opt.optname.should == Socket::SO_LINGER
+    end
+
+    it 'returns a Socket::Option for arguments :TCP and :REPAIR' do
+      opt = @sock.getsockopt(:TCP, :REPAIR)
+
+      opt.level.should   == Socket::SOL_TCP
+      opt.optname.should == Socket::TCP_REPAIR
+    end
+
+    it 'returns a Socket::Option for arguments :UDP and :CORK' do
+      sock = Socket.new(:INET, :DGRAM)
+      opt  = sock.getsockopt(:UDP, :CORK)
+
+      opt.level.should   == Socket::SOL_UDP
+      opt.optname.should == Socket::UDP_CORK
     end
   end
 
-  it "gets a socket option Socket::SO_SNDBUF" do
-    n = @sock.getsockopt(Socket::SOL_SOCKET, Socket::SO_SNDBUF).to_s
-    n.unpack('i')[0].should > 0
+  describe 'using Strings as arguments' do
+    it 'returns a Socket:"O"ption for arguments "SOCKET" and "TYPE"' do
+      opt = @sock.getsockopt("SOCKET", "TYPE")
+
+      opt.level.should   == Socket::SOL_SOCKET
+      opt.optname.should == Socket::SO_TYPE
+    end
+
+    it 'returns a Socket:"O"ption for arguments "IP" and "TTL"' do
+      opt = @sock.getsockopt("IP", "TTL")
+
+      opt.level.should   == Socket::SOL_IP
+      opt.optname.should == Socket::IP_TTL
+    end
+
+    it 'returns a Socket:"O"ption for arguments "IP" and "MULTICAST"_TTL' do
+      opt = @sock.getsockopt("IP", "MULTICAST_TTL")
+
+      opt.level.should   == Socket::SOL_IP
+      opt.optname.should == Socket::IP_MULTICAST_TTL
+    end
+
+    it 'returns a Socket:"O"ption for arguments "SOCKET" and "REUSEADDR"' do
+      opt = @sock.getsockopt("SOCKET", "REUSEADDR")
+
+      opt.level.should   == Socket::SOL_SOCKET
+      opt.optname.should == Socket::SO_REUSEADDR
+    end
+
+    it 'returns a Socket:"O"ption for arguments "SOCKET" and "LINGER"' do
+      opt = @sock.getsockopt("SOCKET", "LINGER")
+
+      opt.level.should   == Socket::SOL_SOCKET
+      opt.optname.should == Socket::SO_LINGER
+    end
+
+    it 'returns a Socket:"O"ption for arguments "TCP" and "REPAIR"' do
+      opt = @sock.getsockopt("TCP", "REPAIR")
+
+      opt.level.should   == Socket::SOL_TCP
+      opt.optname.should == Socket::TCP_REPAIR
+    end
+
+    it 'returns a Socket:"O"ption for arguments "UDP" and "CORK"' do
+      sock = Socket.new("INET", "DGRAM")
+      opt  = sock.getsockopt("UDP", "CORK")
+
+      opt.level.should   == Socket::SOL_UDP
+      opt.optname.should == Socket::UDP_CORK
+    end
   end
 
-  it "raises a SystemCallError with an invalid socket option" do
-    lambda { @sock.getsockopt Socket::SOL_SOCKET, -1 }.should raise_error(Errno::ENOPROTOOPT)
-  end
+  describe 'using a String based option' do
+    it 'allows unpacking of a boolean option' do
+      opt = @sock.getsockopt(Socket::SOL_SOCKET, Socket::SO_REUSEADDR).to_s
 
-  ruby_version_is "1.9.2" do
-    it "gets a Socket::Option" do
-      so = @sock.getsockopt(Socket::SOL_SOCKET, Socket::SO_TYPE)
-      so.class.should == Socket::Option
-      so.level.should == Socket::SOL_SOCKET
-      so.optname.should == Socket::SO_TYPE
-      so.int.should == Socket::SOCK_STREAM
+      opt.unpack('i').should == [0]
+    end
 
-      so = @sock.getsockopt(Socket::SOL_SOCKET, Socket::SO_LINGER)
-      so.optname.should == Socket::SO_LINGER
-      so.linger.should == [false, 0]
+    it 'allows unpacking of a numeric option' do
+      opt   = @sock.getsockopt(Socket::IPPROTO_IP, Socket::IP_TTL).to_s
+      array = opt.unpack('i')
+
+      array[0].should be_an_instance_of(Fixnum)
+      array[0].should > 0
+    end
+
+    it 'allows unpacking of a struct option' do
+      opt = @sock.getsockopt(Socket::SOL_SOCKET, Socket::SO_LINGER).to_s
+
+      if opt.bytesize == 8
+        opt.unpack('ii').should == [0, 0]
+      else
+        opt.unpack('i').should == [0]
+      end
     end
   end
 end
