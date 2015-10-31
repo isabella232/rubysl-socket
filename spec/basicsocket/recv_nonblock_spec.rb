2d9f58d156d9ed2a0eda6f5b01a6f327407dc712
Yorick Peterse <yorickpeterse@gmail.com>
2015-10-31 17:59:21 +0100

    Revamp BasicSocket#recv_nonblock specs
    There's no point in using shared examples if 1) they're not shared 2)
    they can't be shared as they use hardcoded methods names.

--- spec/basicsocket/recv_nonblock_spec.rb
+++ spec/basicsocket/recv_nonblock_spec.rb
@@ -1,6 +1,43 @@
-require File.expand_path('../../shared/recv_nonblock', __FILE__)
-require File.expand_path('../../fixtures/classes', __FILE__)
+require 'socket'
 
-describe "BasicSocket#recv_nonblock" do
-  it_behaves_like :socket_recv_nonblock, :recv_nonblock
+describe 'BasicSocket#recv_nonblock' do
+  before do
+    @server = Socket.new(:INET, :DGRAM)
+    @client = Socket.new(:INET, :DGRAM)
+  end
+
+  after do
+    @client.close
+    @server.close
+  end
+
+  describe 'using an unbound socket' do
+    it 'raises IO::EAGAINWaitReadable' do
+      proc { @server.recv_nonblock(1) }
+        .should raise_error(IO::EAGAINWaitReadable)
+    end
+  end
+
+  describe 'using a bound socket' do
+    before do
+      @server.bind(Socket.sockaddr_in(0, '127.0.0.1'))
+
+      @client.connect(@server.getsockname)
+    end
+
+    describe 'without any data available' do
+      it 'raises IO::EAGAINWaitReadable' do
+        proc { @server.recv_nonblock(1) }
+          .should raise_error(IO::EAGAINWaitReadable)
+      end
+    end
+
+    describe 'with data available' do
+      it 'returns the given amount of bytes' do
+        @client.write('hello')
+
+        @server.recv_nonblock(2).should == 'he'
+      end
+    end
+  end
 end
