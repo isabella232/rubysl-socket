82f6a48f08da0ea62f73a21c2393564fb1406267
Yorick Peterse <yorickpeterse@gmail.com>
2015-11-06 16:27:21 +0100

    Added AncillaryData.unix_rights / #unix_rights

--- lib/socket/ancillary_data.rb
+++ lib/socket/ancillary_data.rb
@@ -6,6 +6,26 @@ class Socket < BasicSocket
       new(family, level, type, [integer].pack('I'))
     end
 
+    def self.unix_rights(*ios)
+      descriptors = ios.map do |io|
+        unless io.is_a?(IO)
+          raise TypeError, "IO expected, got #{io.class} instead"
+        end
+
+        io.fileno
+      end
+
+      instance = new(:UNIX, :SOCKET, :RIGHTS, descriptors.pack('I*'))
+
+      # MRI sets this using a hidden instance variable ("unix_rights"). Because
+      # you can't set hidden instance variables from within Ruby we'll just
+      # prefix the variable with an underscore. Lets hope people don't mess with
+      # it.
+      instance.instance_variable_set(:@_unix_rights, ios)
+
+      instance
+    end
+
     def initialize(family, level, type, data)
       @family = RubySL::Socket::Helpers.address_family(family)
       @data   = RubySL::Socket::Helpers.coerce_to_string(data)
@@ -28,6 +48,14 @@ class Socket < BasicSocket
       end
 
       unpacked
+    end
+
+    def unix_rights
+      if @level != Socket::SOL_SOCKET or @type != Socket::SCM_RIGHTS
+        raise TypeError, 'SCM_RIGHTS ancillary data expected'
+      end
+
+      @_unix_rights
     end
   end
 end
