4a6d303049abdcabc2f223dac3b58b80e7003ff8
Yorick Peterse <yorickpeterse@gmail.com>
2015-11-07 23:59:02 +0100

    Don't prefix "hidden" ivars with an underscore
    In hindsight this is rather pointless as people can still mess with it.

--- lib/socket/ancillary_data.rb
+++ lib/socket/ancillary_data.rb
@@ -18,10 +18,9 @@ class Socket < BasicSocket
       instance = new(:UNIX, :SOCKET, :RIGHTS, descriptors.pack('I*'))
 
       # MRI sets this using a hidden instance variable ("unix_rights"). Because
-      # you can't set hidden instance variables from within Ruby we'll just
-      # prefix the variable with an underscore. Lets hope people don't mess with
-      # it.
-      instance.instance_variable_set(:@_unix_rights, ios)
+      # you can't set hidden instance variables from within Ruby we'll have to
+      # use a regular instance variable. Lets hope people don't mess with it.
+      instance.instance_variable_set(:@unix_rights, ios)
 
       instance
     end
@@ -36,7 +35,7 @@ class Socket < BasicSocket
         Addrinfo.ip(spec_dst.ip_address)
       ]
 
-      instance.instance_variable_set(:@_ip_pktinfo, pkt_info)
+      instance.instance_variable_set(:@ip_pktinfo, pkt_info)
 
       instance
     end
@@ -45,7 +44,7 @@ class Socket < BasicSocket
       instance = new(:INET6, :IPV6, :PKTINFO, '')
       pkt_info = [Addrinfo.ip(addr.ip_address), ifindex]
 
-      instance.instance_variable_set(:@_ipv6_pktinfo, pkt_info)
+      instance.instance_variable_set(:@ipv6_pktinfo, pkt_info)
 
       instance
     end
@@ -79,11 +78,11 @@ class Socket < BasicSocket
         raise TypeError, 'SCM_RIGHTS ancillary data expected'
       end
 
-      @_unix_rights
+      @unix_rights
     end
 
     def data
-      if @_ip_pktinfo or @_ipv6_pktinfo
+      if @ip_pktinfo or @ipv6_pktinfo
         raise NotImplementedError,
           'AncillaryData#data is not supported as its output depends on ' \
           'MRI specific internals, use #ip_pktinfo or #ipv6_pktinfo instead'
@@ -93,13 +92,13 @@ class Socket < BasicSocket
     end
 
     def ip_pktinfo
-      addr, ifindex, spec = @_ip_pktinfo
+      addr, ifindex, spec = @ip_pktinfo
 
       [addr.dup, ifindex, spec.dup]
     end
 
     def ipv6_pktinfo
-      addr, ifindex = @_ipv6_pktinfo
+      addr, ifindex = @ipv6_pktinfo
 
       [addr.dup, ifindex]
     end
