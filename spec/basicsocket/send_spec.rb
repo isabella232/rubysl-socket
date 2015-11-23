63d4fc3e789b6c87b506de69b271cf768bc12291
Yorick Peterse <yorickpeterse@gmail.com>
2015-11-23 15:21:31 +0100

    Revamp BasicSocket#send specs

--- spec/basicsocket/send_spec.rb
+++ spec/basicsocket/send_spec.rb
@@ -1,81 +1,119 @@
+require 'socket'
 require File.expand_path('../../fixtures/classes', __FILE__)
 
-describe "BasicSocket#send" do
-  before :each do
-    @server = TCPServer.new('127.0.0.1', SocketSpecs.port)
-    @socket = TCPSocket.new('127.0.0.1', SocketSpecs.port)
+describe 'BasicSocket#send' do
+  describe 'using a disconnected socket' do
+    before do
+      @client = Socket.new(:INET, :DGRAM)
+      @server = Socket.new(:INET, :DGRAM)
+
+      @server.bind(Socket.sockaddr_in(0, '127.0.0.1'))
+    end
+
+    after do
+      @client.close
+      @server.close
+    end
+
+    describe 'without a destination address' do
+      it 'raises Errno::EDESTADDRREQ' do
+        proc { @client.send('hello', 0) }.should raise_error(Errno::EDESTADDRREQ)
+      end
+    end
+
+    describe 'with a destination address as a String' do
+      it 'returns the amount of sent bytes' do
+        @client.send('hello', 0, @server.getsockname).should == 5
+      end
+
+      it 'does not persist the connection after writing to the socket' do
+        @client.send('hello', 0, @server.getsockname)
+
+        proc { @client.send('hello', 0) }.should raise_error(Errno::EDESTADDRREQ)
+      end
+    end
+
+    describe 'with a destination address as an Addrinfo' do
+      it 'returns the amount of sent bytes' do
+        @client.send('hello', 0, @server.connect_address).should == 5
+      end
+    end
   end
 
-  after :each do
-    @server.closed?.should be_false
-    @socket.closed?.should be_false
+  describe 'using a connected UDP socket' do
+    before do
+      @client = Socket.new(:INET, :DGRAM)
+      @server = Socket.new(:INET, :DGRAM)
 
-    @server.close
-    @socket.close
+      @server.bind(Socket.sockaddr_in(0, '127.0.0.1'))
+      @client.connect(@server.getsockname)
+    end
+
+    after do
+      @client.close
+      @server.close
+    end
+
+    describe 'without a destination address argument' do
+      it 'returns the amount of bytes written' do
+        @client.send('hello', 0).should == 5
+      end
+    end
+
+    describe 'with a destination address argument' do
+      before do
+        @alt_server = Socket.new(:INET, :DGRAM)
+
+        @alt_server.bind(Socket.sockaddr_in(0, '127.0.0.1'))
+      end
+
+      after do
+        @alt_server.close
+      end
+
+      it 'sends the message to the given address instead' do
+        @client.send('hello', 0, @alt_server.getsockname).should == 5
+
+        SocketSpecs.blocking? { @server.recv(5) }.should == true
+
+        @alt_server.recv(5).should == 'hello'
+      end
+
+      it 'does not persist the alternative connection after writing to the socket' do
+        @client.send('hello', 0, @alt_server.getsockname)
+        @client.send('world', 0)
+
+        @server.recv(5).should == 'world'
+      end
+    end
   end
 
-   it "sends a message to another socket and returns the number of bytes sent" do
-     data = ""
-     t = Thread.new do
-       client = @server.accept
-       loop do
-         got = client.recv(5)
-         break if got.empty?
-         data << got
-       end
-       client.close
-     end
-     Thread.pass while t.status and t.status != "sleep"
-     t.status.should_not be_nil
+  describe 'using a connected TPC socket' do
+    before do
+      @client = Socket.new(:INET, :STREAM)
+      @server = Socket.new(:INET, :STREAM)
 
-     @socket.send('hello', 0).should == 5
-     @socket.shutdown(1) # indicate, that we are done sending
-     @socket.recv(10)
+      @server.bind(Socket.sockaddr_in(0, '127.0.0.1'))
+      @server.listen(1)
 
-     t.join
-     data.should == 'hello'
-   end
+      @client.connect(@server.getsockname)
+    end
 
-   it "accepts flags to specify unusual sending behaviour" do
-     data = nil
-     peek_data = nil
-     t = Thread.new do
-       client = @server.accept
-       peek_data = client.recv(6, Socket::MSG_PEEK)
-       data = client.recv(6)
-       client.recv(10) # this recv is important
-       client.close
-     end
-     Thread.pass while t.status and t.status != "sleep"
-     t.status.should_not be_nil
+    after do
+      @client.close
+      @server.close
+    end
 
-     @socket.send('helloU', Socket::MSG_PEEK | Socket::MSG_OOB).should == 6
-     @socket.shutdown # indicate, that we are done sending
+    describe 'using the MSG_OOB flag' do
+      it 'sends an out-of-band message' do
+        @server.setsockopt(:SOCKET, :OOBINLINE, true)
 
-     t.join
-     peek_data.should == "hello"
-     data.should == 'hello'
-   end
+        @client.send('a', Socket::MSG_OOB).should == 1
 
-  it "accepts a sockaddr as recipient address" do
-     data = ""
-     t = Thread.new do
-       client = @server.accept
-       loop do
-         got = client.recv(5)
-         break if got.empty?
-         data << got
-       end
-       client.close
-     end
-     Thread.pass while t.status and t.status != "sleep"
-     t.status.should_not be_nil
+        socket, _ = @server.accept
 
-     sockaddr = Socket.pack_sockaddr_in(SocketSpecs.port, "127.0.0.1")
-     @socket.send('hello', 0, sockaddr).should == 5
-     @socket.shutdown # indicate, that we are done sending
-
-     t.join
-     data.should == 'hello'
+        socket.recv(1, Socket::MSG_OOB).should == 'a'
+      end
+    end
   end
 end
