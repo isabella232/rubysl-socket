07dff015973e2ce30bfa12d1a9e1b5aa81ffe71a
Yorick Peterse <yorickpeterse@gmail.com>
2015-11-27 16:29:12 +0100

    Cleaned up Socket#bind specs

--- spec/socket/bind_spec.rb
+++ spec/socket/bind_spec.rb
@@ -1,124 +1,59 @@
-require File.expand_path('../../fixtures/classes', __FILE__)
+require 'socket'
 
-describe "Socket#bind on SOCK_DGRAM socket" do
-  before do
-    @sock = Socket.new(Socket::AF_INET, Socket::SOCK_DGRAM, 0);
-    @sockaddr = Socket.pack_sockaddr_in(SocketSpecs.port, "127.0.0.1");
-  end
-
-  after do
-    @sock.closed?.should be_false
-    @sock.close
-  end
-
-  it "binds to a port" do
-    lambda { @sock.bind(@sockaddr) }.should_not raise_error
-  end
-
-  it "returns 0 if successful" do
-    @sock.bind(@sockaddr).should == 0
-  end
-
-  it "raises Errno::EINVAL when binding to an already bound port" do
-    @sock.bind(@sockaddr);
-
-    lambda { @sock.bind(@sockaddr); }.should raise_error(Errno::EINVAL);
-  end
-
-  it "raises Errno::EADDRNOTAVAIL when the specified sockaddr is not available from the local machine" do
-    sockaddr1 = Socket.pack_sockaddr_in(SocketSpecs.port, "4.3.2.1");
-
-    lambda { @sock.bind(sockaddr1); }.should raise_error(Errno::EADDRNOTAVAIL)
-  end
-
-  platform_is_not :os => [:windows, :cygwin] do
-    it "raises Errno::EACCES when the current user does not have permission to bind" do
-      sockaddr1 = Socket.pack_sockaddr_in(1, "127.0.0.1");
-
-      lambda { @sock.bind(sockaddr1); }.should raise_error(Errno::EACCES)
-    end
-  end
-end
-
-describe "Socket#bind on SOCK_STREAM socket" do
-  before do
-    @sock = Socket.new(Socket::AF_INET, Socket::SOCK_STREAM, 0);
-    @sock.setsockopt(Socket::SOL_SOCKET, Socket::SO_REUSEADDR, true)
-
-    @sockaddr = Socket.pack_sockaddr_in(SocketSpecs.port, "127.0.0.1");
-  end
-
-  after do
-    @sock.closed?.should be_false
-    @sock.close
-  end
-
-  it "binds to a port" do
-    lambda { @sock.bind(@sockaddr) }.should_not raise_error
-  end
-
-  it "returns 0 if successful" do
-    @sock.bind(@sockaddr).should == 0
-  end
-
-  it "raises Errno::EINVAL when binding to an already bound port" do
-    @sock.bind(@sockaddr);
-
-    lambda { @sock.bind(@sockaddr); }.should raise_error(Errno::EINVAL);
-  end
-
-  it "raises Errno::EADDRNOTAVAIL when the specified sockaddr is not available from the local machine" do
-    sockaddr1 = Socket.pack_sockaddr_in(SocketSpecs.port, "4.3.2.1");
-
-    lambda { @sock.bind(sockaddr1); }.should raise_error(Errno::EADDRNOTAVAIL)
-  end
-
-  platform_is_not :os => [:windows, :cygwin] do
-    it "raises Errno::EACCES when the current user does not have permission to bind" do
-      sockaddr1 = Socket.pack_sockaddr_in(1, "127.0.0.1");
-
-      lambda { @sock.bind(sockaddr1); }.should raise_error(Errno::EACCES)
-    end
-  end
-end
-
-describe 'Socket#bind using an Addrinfo' do
-  before do
-    @addr = Addrinfo.tcp('127.0.0.1', 9999)
-    @sock = Socket.new(@addr.afamily, @addr.socktype)
-  end
-
-  after do
-    @sock.close
-  end
-
-  it 'binds to an Addrinfo' do
-    @sock.bind(@addr)
-
-    @sock.local_address.should be_an_instance_of(Addrinfo)
-  end
-
-  it 'uses a new Addrinfo for the local address' do
-    @sock.bind(@addr)
-
-    @sock.local_address.should_not == @addr
-  end
-
-  describe 'the Addrinfo used as the local address' do
+describe 'Socket#bind' do
+  describe 'using a packed socket address' do
     before do
-      @sock.bind(@addr)
+      @socket   = Socket.new(:INET, :DGRAM)
+      @sockaddr = Socket.sockaddr_in(0, '127.0.0.1')
     end
 
-    it 'has the same address family' do
-      @sock.local_address.afamily.should == @addr.afamily
+    after do
+      @socket.close
     end
 
-    it 'has the same protocol family' do
-      @sock.local_address.pfamily.should == @addr.pfamily
+    it 'returns 0 when successfully bound' do
+      @socket.bind(@sockaddr).should == 0
     end
 
-    it 'has the same socket type' do
-      @sock.local_address.socktype.should == @addr.socktype
+    it 'raises Errno::EINVAL when binding to an already bound port' do
+      @socket.bind(@sockaddr)
+
+      proc { @socket.bind(@sockaddr) }.should raise_error(Errno::EINVAL)
+    end
+
+    it 'raises Errno::EADDRNOTAVAIL when the specified sockaddr is not available' do
+      sockaddr1 = Socket.sockaddr_in(0, '4.3.2.1');
+
+      proc { @socket.bind(sockaddr1); }.should raise_error(Errno::EADDRNOTAVAIL)
+    end
+
+    it 'raises Errno::EACCES when the user is not allowed to bind to the port' do
+      sockaddr1 = Socket.pack_sockaddr_in(1, '127.0.0.1');
+
+      proc { @socket.bind(sockaddr1); }.should raise_error(Errno::EACCES)
+    end
+  end
+
+  describe 'using an Addrinfo' do
+    before do
+      @addr   = Addrinfo.udp('127.0.0.1', 0)
+      @socket = Socket.new(@addr.afamily, @addr.socktype)
+    end
+
+    after do
+      @socket.close
+    end
+
+    it 'binds to an Addrinfo' do
+      @socket.bind(@addr).should == 0
+
+      @socket.local_address.should be_an_instance_of(Addrinfo)
+    end
+
+    it 'uses a new Addrinfo for the local address' do
+      @socket.bind(@addr)
+
+      @socket.local_address.should_not == @addr
     end
   end
 end
