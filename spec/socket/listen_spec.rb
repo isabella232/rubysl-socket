25da29ebd87f7fe46ef4058564fc8d23164c0c76
Yorick Peterse <yorickpeterse@gmail.com>
2015-11-27 17:34:05 +0100

    Revamp Socket#listen specs

--- spec/socket/listen_spec.rb
+++ spec/socket/listen_spec.rb
@@ -1,20 +1,33 @@
-require File.expand_path('../../fixtures/classes', __FILE__)
+require 'socket'
 
-describe "Socket#listen" do
-  before do
-    @socket = Socket.new(Socket::AF_INET, Socket::SOCK_STREAM, 0)
+describe 'Socket#listen' do
+  describe 'using a DGRAM socket' do
+    before do
+      @server = Socket.new(:INET, :DGRAM)
+      @client = Socket.new(:INET, :DGRAM)
+
+      @server.bind(Socket.sockaddr_in(0, '127.0.0.1'))
+    end
+
+    it 'raises Errno::EOPNOTSUPP' do
+      proc { @server.listen(1) }.should raise_error(Errno::EOPNOTSUPP)
+    end
   end
 
-  after do
-    @socket.closed?.should be_false
-    @socket.close
-  end
+  describe 'using a STREAM socket' do
+    before do
+      @server = Socket.new(:INET, :STREAM)
+      @client = Socket.new(:INET, :STREAM)
 
-  it "verifies we can listen for incoming connections" do
-    sockaddr = Socket.pack_sockaddr_in(SocketSpecs.port, "127.0.0.1")
+      @server.bind(Socket.sockaddr_in(0, '127.0.0.1'))
+    end
 
-    @socket.setsockopt(Socket::SOL_SOCKET, Socket::SO_REUSEADDR, true)
-    @socket.bind(sockaddr)
-    @socket.listen(1).should == 0
+    it 'returns 0' do
+      @server.listen(1).should == 0
+    end
+
+    it "raises when the given argument can't be coerced to a Fixnum" do
+      proc { @server.listen('cats') }.should raise_error(TypeError)
+    end
   end
 end
