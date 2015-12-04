76784d0d2e29df47354c9890ccb3a1a67d0bb727
Yorick Peterse <yorickpeterse@gmail.com>
2015-12-04 15:18:34 +0100

    Removed more useless TCPServer specs

--- spec/tcpserver/readpartial_spec.rb
+++
@@ -1,8 +0,0 @@
-require File.expand_path('../../fixtures/classes', __FILE__)
-
-describe "TCPServer#readpartial" do
-  after(:each) do
-    @server.close if @server
-    @socket.close if @socket
-  end
-end
