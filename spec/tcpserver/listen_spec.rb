3d9bbd2e47191b3a7fcc5af6f53232e600b4ae07
Yorick Peterse <yorickpeterse@gmail.com>
2015-12-04 14:21:44 +0100

    Added specs for TCPServer#listen

--- spec/tcpserver/listen_spec.rb
+++ spec/tcpserver/listen_spec.rb
@@ -1,1 +1,19 @@
-require File.expand_path('../../fixtures/classes', __FILE__)
+require 'socket'
+
+describe 'TCPServer#listen' do
+  before do
+    @server = TCPServer.new('127.0.0.1', 0)
+  end
+
+  after do
+    @server.close
+  end
+
+  it 'returns 0' do
+    @server.listen(1).should == 0
+  end
+
+  it "raises when the given argument can't be coerced to a Fixnum" do
+    proc { @server.listen('cats') }.should raise_error(TypeError)
+  end
+end
