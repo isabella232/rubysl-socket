2216af689894d457ca781fd8e400fce5406c1a82
Yorick Peterse <yorickpeterse@gmail.com>
2015-12-11 17:31:12 +0100

    Revamp UNIXSocket#addr specs

--- spec/unixsocket/addr_spec.rb
+++ spec/unixsocket/addr_spec.rb
@@ -1,37 +1,28 @@
-require File.expand_path('../../fixtures/classes', __FILE__)
+require 'socket'
 
-describe "UNIXSocket#addr" do
+describe 'UNIXSocket#addr' do
+  before do
+    @path   = tmp('unix_socket')
+    @server = UNIXServer.new(@path)
+    @client = UNIXSocket.new(@path)
+  end
 
-  platform_is_not :windows do
-    before :each do
-      @path = SocketSpecs.socket_path
-      rm_r @path
+  after do
+    @client.close
+    @server.close
 
-      @server = UNIXServer.open(@path)
-      @client = UNIXSocket.open(@path)
-    end
+    rm_r(@path)
+  end
 
-    after :each do
-      @client.close
-      @server.close
-      rm_r @path
-    end
-
-    it "returns the address family of this socket in an array" do
-      @client.addr[0].should == "AF_UNIX"
-    end
-
-    it "returns the path of the socket in an array if it's a server" do
-      @server.addr[1].should == @path
-    end
-
-    it "returns an empty string for path if it's a client" do
-      @client.addr[1].should == ""
-    end
-
-    it "returns an array" do
-      @client.addr.should be_kind_of(Array)
+  describe 'for the client socket' do
+    it 'returns an Array containing the address family and an empty socket path' do
+      @client.addr.should == ['AF_UNIX', '']
     end
   end
 
+  describe 'for the server socket' do
+    it 'returns an Array containing the address family and the socket path' do
+      @server.addr.should == ['AF_UNIX', @path]
+    end
+  end
 end
