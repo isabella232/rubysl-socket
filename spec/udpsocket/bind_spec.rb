f93ca2a2e608fd79eefcbcfda9b13f59da557742
Yorick Peterse <yorickpeterse@gmail.com>
2015-12-11 12:05:50 +0100

    Added extra specs for UDPSocket#bind

--- spec/udpsocket/bind_spec.rb
+++ spec/udpsocket/bind_spec.rb
@@ -16,9 +16,32 @@ describe 'UDPSocket#bind' do
     @socket.local_address.ip_port.should > 0
   end
 
+  it 'binds to an address and port using String arguments' do
+    @socket.bind('127.0.0.1', '0').should == 0
+
+    @socket.local_address.ip_address.should == '127.0.0.1'
+    @socket.local_address.ip_port.should > 0
+  end
+
   it 'binds to all addresses when the hostname is empty' do
     @socket.bind('', 0).should == 0
 
     @socket.local_address.ip_address.should == '0.0.0.0'
+  end
+
+  it 'can receive data after being bound to an address' do
+    @socket.bind('127.0.0.1', 0)
+
+    addr   = @socket.connect_address
+    client = UDPSocket.new
+
+    client.connect(addr.ip_address, addr.ip_port)
+    client.write('hello')
+
+    begin
+      @socket.recv(6).should == 'hello'
+    ensure
+      client.close
+    end
   end
 end
