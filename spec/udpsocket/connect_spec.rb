b326ef708fbfb13cdc167d2859f2f3ad0966c60b
Yorick Peterse <yorickpeterse@gmail.com>
2015-12-11 12:17:47 +0100

    Added specs for UDPSocket#connect

--- spec/udpsocket/connect_spec.rb
+++ spec/udpsocket/connect_spec.rb
@@ -1,1 +1,32 @@
-require File.expand_path('../../fixtures/classes', __FILE__)
+require 'socket'
+
+describe 'UDPSocket#connect' do
+  before do
+    @socket = UDPSocket.new
+  end
+
+  after do
+    @socket.close
+  end
+
+  it 'connects to an address even when it is not used' do
+    @socket.connect('127.0.0.1', 0).should == 0
+  end
+
+  it 'can send data after connecting' do
+    receiver = UDPSocket.new
+
+    receiver.bind('127.0.0.1', 0)
+
+    addr = receiver.connect_address
+
+    @socket.connect(addr.ip_address, addr.ip_port)
+    @socket.write('hello')
+
+    begin
+      receiver.recv(6).should == 'hello'
+    ensure
+      receiver.close
+    end
+  end
+end
