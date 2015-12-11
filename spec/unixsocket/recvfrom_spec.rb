bbc42ac6590adca86083dfabba8dd776b47dd9a3
Yorick Peterse <yorickpeterse@gmail.com>
2015-12-11 19:17:20 +0100

    Revamp UNIXSocket#recvfrom specs

--- spec/unixsocket/recvfrom_spec.rb
+++ spec/unixsocket/recvfrom_spec.rb
@@ -1,48 +1,77 @@
-require File.expand_path('../../fixtures/classes', __FILE__)
+require 'socket'
 
-describe "UNIXSocket#recvfrom" do
-
-  platform_is_not :windows do
-    before :each do
-      @path = SocketSpecs.socket_path
-      rm_r @path
-
-      @server = UNIXServer.open(@path)
-      @client = UNIXSocket.open(@path)
+describe 'UNIXSocket#recvfrom' do
+  describe 'using regular UNIX sockets' do
+    before do
+      @path   = tmp('unix_socket')
+      @server = UNIXServer.new(@path)
+      @client = UNIXSocket.new(@path)
     end
 
-    after :each do
+    after do
       @client.close
       @server.close
-      rm_r @path
+
+      rm_r(@path)
     end
 
-    it "receives len bytes from sock" do
-      @client.send("foobar", 0)
-      sock = @server.accept
-      sock.recvfrom(6).first.should == "foobar"
-      sock.close
-    end
+    it 'returns an Array containing the data and address information' do
+      @client.write('hello')
 
-    it "returns an array with data and information on the sender" do
-      @client.send("foobar", 0)
-      sock = @server.accept
-      data = sock.recvfrom(6)
-      data.first.should == "foobar"
-      data.last.should == ["AF_UNIX", ""]
-      sock.close
-    end
+      connection = @server.accept
 
-    it "uses different message options" do
-      @client.send("foobar", Socket::MSG_PEEK)
-      sock = @server.accept
-      peek_data = sock.recvfrom(6, Socket::MSG_PEEK) # Does not retrieve the message
-      real_data = sock.recvfrom(6)
-
-      real_data.should == peek_data
-      peek_data.should == ["foobar", ["AF_UNIX", ""]]
-      sock.close
+      begin
+        connection.recvfrom(5).should == ['hello', ['AF_UNIX', '']]
+      ensure
+        connection.close
+      end
     end
   end
 
+  describe 'using a socket pair' do
+    before do
+      @client, @server = UNIXSocket.socketpair
+
+      @client.write('hello')
+    end
+
+    after do
+      @client.close
+      @server.close
+    end
+
+    it 'returns an Array containing the data and address information' do
+      @server.recvfrom(5).should == ['hello', ['AF_UNIX', '']]
+    end
+  end
+
+  # These specs are taken from the rdoc examples on UNIXSocket#recvfrom.
+  describe 'using a UNIX socket constructed using UNIXSocket.for_fd' do
+    before do
+      @path1 = tmp('unix_socket1')
+      @path2 = tmp('unix_socket2')
+
+      @client_raw = Socket.new(:UNIX, :DGRAM)
+      @client_raw.bind(Socket.sockaddr_un(@path1))
+
+      @server_raw = Socket.new(:UNIX, :DGRAM)
+      @server_raw.bind(Socket.sockaddr_un(@path2))
+
+      @socket = UNIXSocket.for_fd(@server_raw.fileno)
+    end
+
+    after do
+      @client_raw.close
+      @server_raw.close # also closes @socket
+
+      rm_r(@path1)
+      rm_r(@path2)
+    end
+
+    it 'returns an Array containing the data and address information' do
+      @client_raw.send('hello', 0, Socket.sockaddr_un(@path2))
+
+      @socket.recvfrom(5).should == ['hello', ['AF_UNIX', @path1]]
+    end
+  end
 end
