2eee6594cde886cfa67c9587a4cc222c34dbadc0
Yorick Peterse <yorickpeterse@gmail.com>
2015-12-11 11:42:37 +0100

    Revamp specs for UDPSocket#bind

--- spec/udpsocket/bind_spec.rb
+++ spec/udpsocket/bind_spec.rb
@@ -1,33 +1,24 @@
-require File.expand_path('../../fixtures/classes', __FILE__)
+require 'socket'
 
-describe "UDPSocket.bind" do
-
-  before :each do
+describe 'UDPSocket#bind' do
+  before do
     @socket = UDPSocket.new
   end
 
-  after :each do
-    @socket.close unless @socket.closed?
+  after do
+    @socket.close
   end
 
-  it "binds the socket to a port" do
-    @socket.bind(SocketSpecs.hostname, SocketSpecs.port)
+  it 'binds to an address and port' do
+    @socket.bind('127.0.0.1', 0).should == 0
 
-    lambda { @socket.bind(SocketSpecs.hostname, SocketSpecs.port) }.should raise_error
+    @socket.local_address.ip_address.should == '127.0.0.1'
+    @socket.local_address.ip_port.should > 0
   end
 
-  it "receives a hostname and a port" do
-    @socket.bind(SocketSpecs.hostname, SocketSpecs.port)
+  it 'binds to all addresses when the hostname is empty' do
+    @socket.bind('', 0).should == 0
 
-    port, host = Socket.unpack_sockaddr_in(@socket.getsockname)
-
-    host.should == "127.0.0.1"
-    port.should == SocketSpecs.port
-  end
-
-  it "binds to INADDR_ANY if the hostname is empty" do
-    @socket.bind("", SocketSpecs.port)
-    port, host = Socket.unpack_sockaddr_in(@socket.getsockname)
-    host.should == "0.0.0.0"
+    @socket.local_address.ip_address.should == '0.0.0.0'
   end
 end
