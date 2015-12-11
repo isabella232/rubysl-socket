1f2c59909e12bfe0c3a2bc21bf827ee9601a0d94
Yorick Peterse <yorickpeterse@gmail.com>
2015-12-11 21:18:01 +0100

    Make Sockaddr_In6 API compatible with Sockaddr_In

--- lib/rubysl/socket/foreign/sockaddr_in6.rb
+++ lib/rubysl/socket/foreign/sockaddr_in6.rb
@@ -4,6 +4,21 @@ module RubySL
       class Sockaddr_In6 < Rubinius::FFI::Struct
         config("rbx.platform.sockaddr_in6", :sin6_family, :sin6_port,
                :sin6_flowinfo, :sin6_addr, :sin6_scope_id)
+
+        def self.with_sockaddr(addr)
+          pointer = Rubinius::FFI::MemoryPointer.new(addr.bytesize)
+          pointer.write_string(addr, addr.bytesize)
+
+          new(pointer)
+        end
+
+        def family
+          self[:sin6_family]
+        end
+
+        def to_s
+          pointer.read_string(pointer.total)
+        end
       end
     end
   end
