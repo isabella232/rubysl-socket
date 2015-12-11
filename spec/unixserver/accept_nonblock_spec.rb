a0d0abfdb91a15550adf572e04294ee9078b625c
Yorick Peterse <yorickpeterse@gmail.com>
2015-12-11 22:10:32 +0100

    Revamp specs for UNIXServer#accept_nonblock

--- spec/unixserver/accept_nonblock_spec.rb
+++ spec/unixserver/accept_nonblock_spec.rb
@@ -1,33 +1,71 @@
-require File.expand_path('../../fixtures/classes', __FILE__)
+require 'socket'
 
-describe "UNIXServer#accept_nonblock" do
+describe 'UNIXServer#accept_nonblock' do
+  before do
+    @path   = tmp('unix_socket')
+    @server = UNIXServer.new(@path)
+  end
 
-  platform_is_not :windows do
-    before :each do
-      @path = SocketSpecs.socket_path
-      rm_r @path
+  after do
+    @server.close
 
-      @server = UNIXServer.open(@path)
-      @client = UNIXSocket.open(@path)
+    rm_r(@path)
+  end
 
-      @socket = @server.accept_nonblock
-      @client.send("foobar", 0)
+  describe 'without a client' do
+    it 'raises IO::EAGAINWaitReadable' do
+      proc { @server.accept_nonblock }
+        .should raise_error(IO::EAGAINWaitReadable)
+    end
+  end
+
+  describe 'with a client' do
+    before do
+      @client = UNIXSocket.new(@path)
     end
 
-    after :each do
-      @socket.close
+    after do
       @client.close
-      @server.close
-      rm_r @path
     end
 
-    it "accepts a connection in a non-blocking way" do
-      data = @socket.recvfrom(6).first
-      data.should == "foobar"
+    describe 'without any data' do
+      it 'returns a UNIXSocket' do
+        socket = @server.accept_nonblock
+
+        begin
+          socket.should be_an_instance_of(UNIXSocket)
+        ensure
+          socket.close
+        end
+      end
     end
 
-    it "returns a UNIXSocket" do
-      @socket.should be_kind_of(UNIXSocket)
+    describe 'with data available' do
+      before do
+        @client.write('hello')
+      end
+
+      it 'returns a UNIXSocket' do
+        socket = @server.accept_nonblock
+
+        begin
+          socket.should be_an_instance_of(UNIXSocket)
+        ensure
+          socket.close
+        end
+      end
+
+      describe 'the returned UNIXSocket' do
+        it 'can read the data written' do
+          socket = @server.accept_nonblock
+
+          begin
+            socket.recv(5).should == 'hello'
+          ensure
+            socket.close
+          end
+        end
+      end
     end
   end
 end
