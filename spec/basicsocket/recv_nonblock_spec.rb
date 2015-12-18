e92867759b0664248aedb9cd1d92f1f27d5426a8
Yorick Peterse <yorickpeterse@gmail.com>
2015-12-18 15:16:38 +0100

    Added IPv6 specs for BasicSocket#recv_nonblock

--- spec/basicsocket/recv_nonblock_spec.rb
+++ spec/basicsocket/recv_nonblock_spec.rb
@@ -1,38 +1,65 @@
 require 'socket'
 
 describe 'BasicSocket#recv_nonblock' do
-  before do
-    @server = Socket.new(:INET, :DGRAM)
-    @client = Socket.new(:INET, :DGRAM)
-  end
-
-  after do
-    @client.close
-    @server.close
-  end
-
-  describe 'using an unbound socket' do
-    it 'raises IO::EAGAINWaitReadable' do
-      proc { @server.recv_nonblock(1) }
-        .should raise_error(IO::EAGAINWaitReadable)
-    end
-  end
-
-  describe 'using a bound socket' do
+  describe 'using IPv4' do
     before do
-      @server.bind(Socket.sockaddr_in(0, '127.0.0.1'))
-
-      @client.connect(@server.getsockname)
+      @server = Socket.new(:INET, :DGRAM)
+      @client = Socket.new(:INET, :DGRAM)
     end
 
-    describe 'without any data available' do
+    after do
+      @client.close
+      @server.close
+    end
+
+    describe 'using an unbound socket' do
       it 'raises IO::EAGAINWaitReadable' do
         proc { @server.recv_nonblock(1) }
           .should raise_error(IO::EAGAINWaitReadable)
       end
     end
 
-    describe 'with data available' do
+    describe 'using a bound socket' do
+      before do
+        @server.bind(Socket.sockaddr_in(0, '127.0.0.1'))
+
+        @client.connect(@server.getsockname)
+      end
+
+      describe 'without any data available' do
+        it 'raises IO::EAGAINWaitReadable' do
+          proc { @server.recv_nonblock(1) }
+            .should raise_error(IO::EAGAINWaitReadable)
+        end
+      end
+
+      describe 'with data available' do
+        it 'returns the given amount of bytes' do
+          @client.write('hello')
+
+          @server.recv_nonblock(2).should == 'he'
+        end
+      end
+    end
+  end
+
+  describe 'using IPv6' do
+    before do
+      @server = Socket.new(:INET6, :DGRAM)
+      @client = Socket.new(:INET6, :DGRAM)
+    end
+
+    after do
+      @client.close
+      @server.close
+    end
+
+    describe 'using a bound socket with data available' do
+      before do
+        @server.bind(Socket.sockaddr_in(0, '::1'))
+        @client.connect(@server.getsockname)
+      end
+
       it 'returns the given amount of bytes' do
         @client.write('hello')
 
