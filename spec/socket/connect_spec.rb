b22df6bfdc6daab834e276a96a2836d4e2bba94e
Yorick Peterse <yorickpeterse@gmail.com>
2015-12-18 22:26:16 +0100

    Added IPv6 specs for Socket#connect

--- spec/socket/connect_spec.rb
+++ spec/socket/connect_spec.rb
@@ -1,41 +1,43 @@
 require 'socket'
 
 describe 'Socket#connect' do
-  before do
-    @server = Socket.new(:INET, :STREAM)
-    @client = Socket.new(:INET, :STREAM)
+  each_ip_protocol do |family, ip_address|
+    before do
+      @server = Socket.new(family, :STREAM)
+      @client = Socket.new(family, :STREAM)
 
-    @server.bind(Socket.sockaddr_in(0, '127.0.0.1'))
-  end
+      @server.bind(Socket.sockaddr_in(0, ip_address))
+    end
 
-  after do
-    @client.close
-    @server.close
-  end
+    after do
+      @client.close
+      @server.close
+    end
 
-  it 'returns 0 when connected successfully using a String' do
-    @server.listen(1)
+    it 'returns 0 when connected successfully using a String' do
+      @server.listen(1)
 
-    @client.connect(@server.getsockname).should == 0
-  end
+      @client.connect(@server.getsockname).should == 0
+    end
 
-  it 'returns 0 when connected successfully using an Addrinfo' do
-    @server.listen(1)
+    it 'returns 0 when connected successfully using an Addrinfo' do
+      @server.listen(1)
 
-    @client.connect(@server.connect_address).should == 0
-  end
+      @client.connect(@server.connect_address).should == 0
+    end
 
-  it 'raises Errno::EISCONN when already connected' do
-    @server.listen(1)
+    it 'raises Errno::EISCONN when already connected' do
+      @server.listen(1)
 
-    @client.connect(@server.getsockname).should == 0
+      @client.connect(@server.getsockname).should == 0
 
-    proc { @client.connect(@server.getsockname) }
-      .should raise_error(Errno::EISCONN)
-  end
+      proc { @client.connect(@server.getsockname) }
+        .should raise_error(Errno::EISCONN)
+    end
 
-  it 'raises Errno::ECONNREFUSED when the connection failed' do
-    proc { @client.connect(@server.getsockname) }
-      .should raise_error(Errno::ECONNREFUSED)
+    it 'raises Errno::ECONNREFUSED when the connection failed' do
+      proc { @client.connect(@server.getsockname) }
+        .should raise_error(Errno::ECONNREFUSED)
+    end
   end
 end
