36f88cf76fed43c1bcbc07bb0c3904af3658757c
Yorick Peterse <yorickpeterse@gmail.com>
2015-12-18 17:41:07 +0100

    Added IPv6 specs for IPSocket#peeraddr

--- spec/ipsocket/peeraddr_spec.rb
+++ spec/ipsocket/peeraddr_spec.rb
@@ -1,66 +1,69 @@
 require 'socket'
 
 describe 'IPSocket#peeraddr' do
-  before do
-    @ip     = '127.0.0.1'
-    @server = TCPServer.new(@ip, 0)
-    @port   = @server.connect_address.ip_port
-    @client = TCPSocket.new(@ip, @port)
-  end
-
-  after do
-    @client.close
-    @server.close
-  end
-
-  describe 'without reverse lookups' do
+  each_ip_protocol do |family, ip_address, family_name|
     before do
-      @hostname = Socket.getaddrinfo(@ip, nil)[0][2]
-    end
-
-    it 'returns an Array containing address information' do
-      @client.peeraddr.should == ['AF_INET', @port, @hostname, @ip]
-    end
-  end
-
-  describe 'with reverse lookups' do
-    before do
-      @hostname = Socket.getaddrinfo(@ip, nil, nil, 0, 0, 0, true)[0][2]
-    end
-
-    describe 'using true as the argument' do
-      it 'returns an Array containing address information' do
-        @client.peeraddr(true).should == ['AF_INET', @port, @hostname, @ip]
-      end
-    end
-
-    describe 'using :hostname as the argument' do
-      it 'returns an Array containing address information' do
-        @client.peeraddr(:hostname).should == ['AF_INET', @port, @hostname, @ip]
-      end
-    end
-
-    describe 'using :cats as the argument' do
-      it 'raises ArgumentError' do
-        proc { @client.peeraddr(:cats) }.should raise_error(ArgumentError)
-      end
-    end
-  end
-
-  describe 'with do_not_reverse_lookup disabled on socket level' do
-    before do
-      @client.do_not_reverse_lookup = false
-
-      @hostname = Socket.getaddrinfo(@ip, nil, nil, 0, 0, 0, true)[0][2]
-      @port     = @client.local_address.ip_port
+      @server = TCPServer.new(ip_address, 0)
+      @port   = @server.connect_address.ip_port
+      @client = TCPSocket.new(ip_address, @port)
     end
 
     after do
-      @client.do_not_reverse_lookup = true
+      @client.close
+      @server.close
     end
 
-    it 'returns an Array containing address information' do
-      @client.addr.should == ['AF_INET', @port, @hostname, @ip]
+    describe 'without reverse lookups' do
+      before do
+        @hostname = Socket.getaddrinfo(ip_address, nil)[0][2]
+      end
+
+      it 'returns an Array containing address information' do
+        @client.peeraddr.should == [family_name, @port, @hostname, ip_address]
+      end
+    end
+
+    describe 'with reverse lookups' do
+      before do
+        @hostname = Socket.getaddrinfo(ip_address, nil, nil, 0, 0, 0, true)[0][2]
+      end
+
+      describe 'using true as the argument' do
+        it 'returns an Array containing address information' do
+          @client.peeraddr(true)
+            .should == [family_name, @port, @hostname, ip_address]
+        end
+      end
+
+      describe 'using :hostname as the argument' do
+        it 'returns an Array containing address information' do
+          @client.peeraddr(:hostname)
+            .should == [family_name, @port, @hostname, ip_address]
+        end
+      end
+
+      describe 'using :cats as the argument' do
+        it 'raises ArgumentError' do
+          proc { @client.peeraddr(:cats) }.should raise_error(ArgumentError)
+        end
+      end
+    end
+
+    describe 'with do_not_reverse_lookup disabled on socket level' do
+      before do
+        @client.do_not_reverse_lookup = false
+
+        @hostname = Socket.getaddrinfo(ip_address, nil, nil, 0, 0, 0, true)[0][2]
+        @port     = @client.local_address.ip_port
+      end
+
+      after do
+        @client.do_not_reverse_lookup = true
+      end
+
+      it 'returns an Array containing address information' do
+        @client.addr.should == [family_name, @port, @hostname, ip_address]
+      end
     end
   end
 end
