de40109cce2d48f2d9efe35c03287c815542acf5
Yorick Peterse <yorickpeterse@gmail.com>
2015-12-18 17:39:04 +0100

    Added IPv6 specs for IPSocket#addr

--- spec/ipsocket/addr_spec.rb
+++ spec/ipsocket/addr_spec.rb
@@ -1,63 +1,65 @@
 require 'socket'
 
 describe 'IPSocket#addr' do
-  before do
-    @ip     = '127.0.0.1'
-    @server = TCPServer.new(@ip, 0)
-    @port   = @server.connect_address.ip_port
-  end
-
-  after do
-    @server.close
-  end
-
-  describe 'without reverse lookups' do
+  each_ip_protocol do |family, ip_address, family_name|
     before do
-      @hostname = Socket.getaddrinfo(@ip, nil)[0][2]
-    end
-
-    it 'returns an Array containing address information' do
-      @server.addr.should == ['AF_INET', @port, @hostname, @ip]
-    end
-  end
-
-  describe 'with reverse lookups' do
-    before do
-      @hostname = Socket.getaddrinfo(@ip, nil, nil, 0, 0, 0, true)[0][2]
-    end
-
-    describe 'using true as the argument' do
-      it 'returns an Array containing address information' do
-        @server.addr(true).should == ['AF_INET', @port, @hostname, @ip]
-      end
-    end
-
-    describe 'using :hostname as the argument' do
-      it 'returns an Array containing address information' do
-        @server.addr(:hostname).should == ['AF_INET', @port, @hostname, @ip]
-      end
-    end
-
-    describe 'using :cats as the argument' do
-      it 'raises ArgumentError' do
-        proc { @server.addr(:cats) }.should raise_error(ArgumentError)
-      end
-    end
-  end
-
-  describe 'with do_not_reverse_lookup disabled on socket level' do
-    before do
-      @server.do_not_reverse_lookup = false
-
-      @hostname = Socket.getaddrinfo(@ip, nil, nil, 0, 0, 0, true)[0][2]
+      @server = TCPServer.new(ip_address, 0)
+      @port   = @server.connect_address.ip_port
     end
 
     after do
-      @server.do_not_reverse_lookup = true
+      @server.close
     end
 
-    it 'returns an Array containing address information' do
-      @server.addr.should == ['AF_INET', @port, @hostname, @ip]
+    describe 'without reverse lookups' do
+      before do
+        @hostname = Socket.getaddrinfo(ip_address, nil)[0][2]
+      end
+
+      it 'returns an Array containing address information' do
+        @server.addr.should == [family_name, @port, @hostname, ip_address]
+      end
+    end
+
+    describe 'with reverse lookups' do
+      before do
+        @hostname = Socket.getaddrinfo(ip_address, nil, nil, 0, 0, 0, true)[0][2]
+      end
+
+      describe 'using true as the argument' do
+        it 'returns an Array containing address information' do
+          @server.addr(true).should == [family_name, @port, @hostname, ip_address]
+        end
+      end
+
+      describe 'using :hostname as the argument' do
+        it 'returns an Array containing address information' do
+          @server.addr(:hostname)
+            .should == [family_name, @port, @hostname, ip_address]
+        end
+      end
+
+      describe 'using :cats as the argument' do
+        it 'raises ArgumentError' do
+          proc { @server.addr(:cats) }.should raise_error(ArgumentError)
+        end
+      end
+    end
+
+    describe 'with do_not_reverse_lookup disabled on socket level' do
+      before do
+        @server.do_not_reverse_lookup = false
+
+        @hostname = Socket.getaddrinfo(ip_address, nil, nil, 0, 0, 0, true)[0][2]
+      end
+
+      after do
+        @server.do_not_reverse_lookup = true
+      end
+
+      it 'returns an Array containing address information' do
+        @server.addr.should == [family_name, @port, @hostname, ip_address]
+      end
     end
   end
 end
