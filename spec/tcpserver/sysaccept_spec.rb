a7e40e840dc86750b0ec6996643099d32d7dfa29
Yorick Peterse <yorickpeterse@gmail.com>
2015-12-23 23:21:53 +0100

    Added IPv6 specs for TCPServer#sysaccept

--- spec/tcpserver/sysaccept_spec.rb
+++ spec/tcpserver/sysaccept_spec.rb
@@ -1,35 +1,36 @@
 require 'socket'
-require File.expand_path('../../fixtures/classes', __FILE__)
 
 describe 'TCPServer#sysaccept' do
-  before do
-    @server = TCPServer.new('127.0.0.1', 0)
-  end
-
-  after do
-    @server.close
-  end
-
-  describe 'without a connected client' do
-    it 'blocks the caller' do
-      proc { @server.sysaccept }.should block_caller
-    end
-  end
-
-  describe 'with a connected client' do
+  each_ip_protocol do |family, ip_address|
     before do
-      @client = TCPSocket.new('127.0.0.1', @server.connect_address.ip_port)
+      @server = TCPServer.new(ip_address, 0)
     end
 
     after do
-      @client.close
+      @server.close
     end
 
-    it 'returns a new file descriptor as a Fixnum' do
-      fd = @server.sysaccept
+    describe 'without a connected client' do
+      it 'blocks the caller' do
+        proc { @server.sysaccept }.should block_caller
+      end
+    end
 
-      fd.should be_an_instance_of(Fixnum)
-      fd.should_not == @client.fileno
+    describe 'with a connected client' do
+      before do
+        @client = TCPSocket.new(ip_address, @server.connect_address.ip_port)
+      end
+
+      after do
+        @client.close
+      end
+
+      it 'returns a new file descriptor as a Fixnum' do
+        fd = @server.sysaccept
+
+        fd.should be_an_instance_of(Fixnum)
+        fd.should_not == @client.fileno
+      end
     end
   end
 end
