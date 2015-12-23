a74d3a7cd73d68c08b66cd2559a98195632b3a0c
Yorick Peterse <yorickpeterse@gmail.com>
2015-12-23 22:56:30 +0100

    Added IPv6 specs for Socket#remote_address

--- spec/socket/remote_address_spec.rb
+++ spec/socket/remote_address_spec.rb
@@ -1,52 +1,53 @@
 require 'socket'
-require File.expand_path('../../fixtures/classes', __FILE__)
 
 describe 'Socket#remote_address' do
-  before do
-    @server = Socket.new(:INET, :STREAM, 0)
+  each_ip_protocol do |family, ip_address|
+    before do
+      @server = Socket.new(family, :STREAM)
 
-    @server.bind(Socket.sockaddr_in(0, '127.0.0.1'))
-    @server.listen(1)
+      @server.bind(Socket.sockaddr_in(0, ip_address))
+      @server.listen(1)
 
-    @host   = @server.local_address.ip_address
-    @port   = @server.local_address.ip_port
-    @client = Socket.new(:INET, :STREAM, Socket::IPPROTO_TCP)
+      @host   = @server.local_address.ip_address
+      @port   = @server.local_address.ip_port
+      @client = Socket.new(family, :STREAM, Socket::IPPROTO_TCP)
 
-    @client.connect(Socket.sockaddr_in(@port, @host))
-  end
-
-  after do
-    @client.close
-    @server.close
-  end
-
-  it 'returns an Addrinfo' do
-    @client.remote_address.should be_an_instance_of(Addrinfo)
-  end
-
-  describe 'the returned Addrinfo' do
-    it 'uses AF_INET as the address family' do
-      @client.remote_address.afamily.should == Socket::AF_INET
+      @client.connect(Socket.sockaddr_in(@port, @host))
     end
 
-    it 'uses PF_INET as the protocol family' do
-      @client.remote_address.pfamily.should == Socket::PF_INET
+    after do
+      @client.close
+      @server.close
     end
 
-    it 'uses SOCK_STREAM as the socket type' do
-      @client.remote_address.socktype.should == Socket::SOCK_STREAM
+    it 'returns an Addrinfo' do
+      @client.remote_address.should be_an_instance_of(Addrinfo)
     end
 
-    it 'uses the correct IP address' do
-      @client.remote_address.ip_address.should == @host
-    end
+    describe 'the returned Addrinfo' do
+      it 'uses AF_INET as the address family' do
+        @client.remote_address.afamily.should == family
+      end
 
-    it 'uses the correct port' do
-      @client.remote_address.ip_port.should == @port
-    end
+      it 'uses PF_INET as the protocol family' do
+        @client.remote_address.pfamily.should == family
+      end
 
-    it 'uses 0 as the protocol' do
-      @client.remote_address.protocol.should == 0
+      it 'uses SOCK_STREAM as the socket type' do
+        @client.remote_address.socktype.should == Socket::SOCK_STREAM
+      end
+
+      it 'uses the correct IP address' do
+        @client.remote_address.ip_address.should == @host
+      end
+
+      it 'uses the correct port' do
+        @client.remote_address.ip_port.should == @port
+      end
+
+      it 'uses 0 as the protocol' do
+        @client.remote_address.protocol.should == 0
+      end
     end
   end
 end
