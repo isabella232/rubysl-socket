b4460a82b9889adba9cd1bf93e773f968eb686ca
Yorick Peterse <yorickpeterse@gmail.com>
2015-12-23 20:24:09 +0100

    Revamp Socket.socketpair specs

--- spec/socket/pair_spec.rb
+++
@@ -1,6 +0,0 @@
-require File.expand_path('../../fixtures/classes', __FILE__)
-require File.expand_path('../../shared/socketpair', __FILE__)
-
-describe "Socket#pair" do
-  it_behaves_like :socket_socketpair, :pair
-end
