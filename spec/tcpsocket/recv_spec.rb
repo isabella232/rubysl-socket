aace2bb5bb0b7c8703927cb0294d6eafef3f516b
Yorick Peterse <yorickpeterse@gmail.com>
2015-12-24 00:17:53 +0100

    Added IPv6 specs for TCPSocket#recv

--- spec/tcpsocket/recv_spec.rb
+++ spec/tcpsocket/recv_spec.rb
@@ -1,25 +1,27 @@
 require 'socket'
 
 describe 'TCPSocket#recv' do
-  before do
-    @server = TCPServer.new('127.0.0.1', 0)
-    @client = TCPSocket.new('127.0.0.1', @server.connect_address.ip_port)
-  end
+  each_ip_protocol do |family, ip_address|
+    before do
+      @server = TCPServer.new(ip_address, 0)
+      @client = TCPSocket.new(ip_address, @server.connect_address.ip_port)
+    end
 
-  after do
-    @client.close
-    @server.close
-  end
+    after do
+      @client.close
+      @server.close
+    end
 
-  it 'returns the message data' do
-    @client.write('hello')
+    it 'returns the message data' do
+      @client.write('hello')
 
-    socket = @server.accept
+      socket = @server.accept
 
-    begin
-      socket.recv(5).should == 'hello'
-    ensure
-      socket.close
+      begin
+        socket.recv(5).should == 'hello'
+      ensure
+        socket.close
+      end
     end
   end
 end
