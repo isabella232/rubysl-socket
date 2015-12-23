5efc4ae58f4f57223a2ac3609baf4fc754f40b82
Yorick Peterse <yorickpeterse@gmail.com>
2015-12-23 23:01:33 +0100

    Added IPv6 Socket#sysaccept specs
    This also removes the use of the SocketSpecs module.

--- spec/socket/sysaccept_spec.rb
+++ spec/socket/sysaccept_spec.rb
@@ -1,74 +1,75 @@
 require 'socket'
-require File.expand_path('../../fixtures/classes', __FILE__)
 
 describe 'Socket#accept' do
-  before do
-    @server   = Socket.new(:INET, :STREAM, 0)
-    @sockaddr = Socket.sockaddr_in(0, '127.0.0.1')
-  end
-
-  after do
-    @server.close
-  end
-
-  describe 'using an unbound socket'  do
-    it 'raises Errno::EINVAL' do
-      proc { @server.sysaccept }.should raise_error(Errno::EINVAL)
-    end
-  end
-
-  describe "using a bound socket that's not listening" do
+  each_ip_protocol do |family, ip_address|
     before do
-      @server.bind(@sockaddr)
+      @server   = Socket.new(family, :STREAM)
+      @sockaddr = Socket.sockaddr_in(0, ip_address)
     end
 
-    it 'raises Errno::EINVAL' do
-      proc { @server.sysaccept }.should raise_error(Errno::EINVAL)
-    end
-  end
-
-  describe "using a bound socket that's listening" do
-    before do
-      @server.bind(@sockaddr)
-      @server.listen(1)
-
-      server_ip    = @server.local_address.ip_port
-      @server_addr = Socket.sockaddr_in(server_ip, '127.0.0.1')
+    after do
+      @server.close
     end
 
-    describe 'without a connected client' do
-      it 'blocks the caller until a connection is available' do
-        client = Socket.new(:INET, :STREAM, 0)
-        thread = Thread.new do
-          @server.sysaccept
+    describe 'using an unbound socket'  do
+      it 'raises Errno::EINVAL' do
+        proc { @server.sysaccept }.should raise_error(Errno::EINVAL)
+      end
+    end
+
+    describe "using a bound socket that's not listening" do
+      before do
+        @server.bind(@sockaddr)
+      end
+
+      it 'raises Errno::EINVAL' do
+        proc { @server.sysaccept }.should raise_error(Errno::EINVAL)
+      end
+    end
+
+    describe "using a bound socket that's listening" do
+      before do
+        @server.bind(@sockaddr)
+        @server.listen(1)
+
+        server_ip    = @server.local_address.ip_port
+        @server_addr = Socket.sockaddr_in(server_ip, ip_address)
+      end
+
+      describe 'without a connected client' do
+        it 'blocks the caller until a connection is available' do
+          client = Socket.new(family, :STREAM)
+          thread = Thread.new do
+            @server.sysaccept
+          end
+
+          client.connect(@server_addr)
+
+          thread.join(5)
+
+          thread.value.should be_an_instance_of(Array)
+        end
+      end
+
+      describe 'with a connected client' do
+        before do
+          @client = Socket.new(family, :STREAM)
+
+          @client.connect(@server.getsockname)
         end
 
-        client.connect(@server_addr)
+        it 'returns an Array containing a Fixnum and an Addrinfo' do
+          fd, addrinfo = @server.sysaccept
 
-        SocketSpecs.join_thread!(thread)
+          fd.should be_an_instance_of(Fixnum)
+          addrinfo.should be_an_instance_of(Addrinfo)
+        end
 
-        thread.value.should be_an_instance_of(Array)
-      end
-    end
+        it 'returns a new file descriptor' do
+          fd, _ = @server.sysaccept
 
-    describe 'with a connected client' do
-      before do
-        @client = Socket.new(:INET, :STREAM)
-
-        @client.connect(@server.getsockname)
-      end
-
-      it 'returns an Array containing a Fixnum and an Addrinfo' do
-        fd, addrinfo = @server.sysaccept
-
-        fd.should be_an_instance_of(Fixnum)
-        addrinfo.should be_an_instance_of(Addrinfo)
-      end
-
-      it 'returns a new file descriptor' do
-        fd, _ = @server.sysaccept
-
-        fd.should_not == @client.fileno
+          fd.should_not == @client.fileno
+        end
       end
     end
   end
