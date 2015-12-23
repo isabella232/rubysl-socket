e8bb494caae67295f4b38ecb0524ca1cc22ef3d8
Yorick Peterse <yorickpeterse@gmail.com>
2015-12-23 23:20:55 +0100

    Added IPv6 specs for TCPServer#listen

--- spec/tcpserver/listen_spec.rb
+++ spec/tcpserver/listen_spec.rb
@@ -1,19 +1,21 @@
 require 'socket'
 
 describe 'TCPServer#listen' do
-  before do
-    @server = TCPServer.new('127.0.0.1', 0)
-  end
+  each_ip_protocol do |family, ip_address|
+    before do
+      @server = TCPServer.new(ip_address, 0)
+    end
 
-  after do
-    @server.close
-  end
+    after do
+      @server.close
+    end
 
-  it 'returns 0' do
-    @server.listen(1).should == 0
-  end
+    it 'returns 0' do
+      @server.listen(1).should == 0
+    end
 
-  it "raises when the given argument can't be coerced to a Fixnum" do
-    proc { @server.listen('cats') }.should raise_error(TypeError)
+    it "raises when the given argument can't be coerced to a Fixnum" do
+      proc { @server.listen('cats') }.should raise_error(TypeError)
+    end
   end
 end
