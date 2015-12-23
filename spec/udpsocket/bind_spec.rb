d151ea4c53f7966ab9eeab09903e2a47cf345fd4
Yorick Peterse <yorickpeterse@gmail.com>
2015-12-24 00:22:02 +0100

    Added IPv6 specs for UDPSocket#bind

--- spec/udpsocket/bind_spec.rb
+++ spec/udpsocket/bind_spec.rb
@@ -1,47 +1,59 @@
 require 'socket'
 
 describe 'UDPSocket#bind' do
-  before do
-    @socket = UDPSocket.new
+  each_ip_protocol do |family, ip_address|
+    before do
+      @socket = UDPSocket.new(family)
+    end
+
+    after do
+      @socket.close
+    end
+
+    it 'binds to an address and port' do
+      @socket.bind(ip_address, 0).should == 0
+
+      @socket.local_address.ip_address.should == ip_address
+      @socket.local_address.ip_port.should > 0
+    end
+
+    it 'binds to an address and port using String arguments' do
+      @socket.bind(ip_address, '0').should == 0
+
+      @socket.local_address.ip_address.should == ip_address
+      @socket.local_address.ip_port.should > 0
+    end
+
+    it 'can receive data after being bound to an address' do
+      @socket.bind(ip_address, 0)
+
+      addr   = @socket.connect_address
+      client = UDPSocket.new(family)
+
+      client.connect(addr.ip_address, addr.ip_port)
+      client.write('hello')
+
+      begin
+        @socket.recv(6).should == 'hello'
+      ensure
+        client.close
+      end
+    end
   end
 
-  after do
-    @socket.close
-  end
+  describe 'using IPv4' do
+    before do
+      @socket = UDPSocket.new
+    end
 
-  it 'binds to an address and port' do
-    @socket.bind('127.0.0.1', 0).should == 0
+    after do
+      @socket.close
+    end
 
-    @socket.local_address.ip_address.should == '127.0.0.1'
-    @socket.local_address.ip_port.should > 0
-  end
+    it 'binds to all addresses when the hostname is empty' do
+      @socket.bind('', 0).should == 0
 
-  it 'binds to an address and port using String arguments' do
-    @socket.bind('127.0.0.1', '0').should == 0
-
-    @socket.local_address.ip_address.should == '127.0.0.1'
-    @socket.local_address.ip_port.should > 0
-  end
-
-  it 'binds to all addresses when the hostname is empty' do
-    @socket.bind('', 0).should == 0
-
-    @socket.local_address.ip_address.should == '0.0.0.0'
-  end
-
-  it 'can receive data after being bound to an address' do
-    @socket.bind('127.0.0.1', 0)
-
-    addr   = @socket.connect_address
-    client = UDPSocket.new
-
-    client.connect(addr.ip_address, addr.ip_port)
-    client.write('hello')
-
-    begin
-      @socket.recv(6).should == 'hello'
-    ensure
-      client.close
+      @socket.local_address.ip_address.should == '0.0.0.0'
     end
   end
 end
