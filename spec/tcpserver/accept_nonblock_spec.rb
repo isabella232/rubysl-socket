887307336e74ea9bb2850358949296853b84465e
Yorick Peterse <yorickpeterse@gmail.com>
2015-12-23 23:10:38 +0100

    Added IPv6 specs for TCPServer#accept_nonblock

--- spec/tcpserver/accept_nonblock_spec.rb
+++ spec/tcpserver/accept_nonblock_spec.rb
@@ -1,31 +1,34 @@
 require 'socket'
 
-describe 'TCPServer#accept' do
-  before do
-    @server = TCPServer.new('127.0.0.1', 0)
-  end
-
-  after do
-    @server.close
-  end
-
-  describe 'without a connected client' do
-    it 'raises IO::EAGAINWaitReadable' do
-      proc { @server.accept_nonblock }.should raise_error(IO::EAGAINWaitReadable)
-    end
-  end
-
-  describe 'with a connected client' do
+describe 'TCPServer#accept_nonblock' do
+  each_ip_protocol do |family, ip_address|
     before do
-      @client = TCPSocket.new('127.0.0.1', @server.connect_address.ip_port)
+      @server = TCPServer.new(ip_address, 0)
     end
 
     after do
-      @client.close
+      @server.close
     end
 
-    it 'returns a TCPSocket' do
-      @server.accept_nonblock.should be_an_instance_of(TCPSocket)
+    describe 'without a connected client' do
+      it 'raises IO::EAGAINWaitReadable' do
+        proc { @server.accept_nonblock }
+          .should raise_error(IO::EAGAINWaitReadable)
+      end
+    end
+
+    describe 'with a connected client' do
+      before do
+        @client = TCPSocket.new(ip_address, @server.connect_address.ip_port)
+      end
+
+      after do
+        @client.close
+      end
+
+      it 'returns a TCPSocket' do
+        @server.accept_nonblock.should be_an_instance_of(TCPSocket)
+      end
     end
   end
 end
