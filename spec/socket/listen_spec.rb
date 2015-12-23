e373220fc032a02cf4840c4765c32fa14d1c27d7
Yorick Peterse <yorickpeterse@gmail.com>
2015-12-23 19:07:56 +0100

    Added IPv6 specs for Socket#listen

--- spec/socket/listen_spec.rb
+++ spec/socket/listen_spec.rb
@@ -1,33 +1,35 @@
 require 'socket'
 
 describe 'Socket#listen' do
-  describe 'using a DGRAM socket' do
-    before do
-      @server = Socket.new(:INET, :DGRAM)
-      @client = Socket.new(:INET, :DGRAM)
+  each_ip_protocol do |family, ip_address|
+    describe 'using a DGRAM socket' do
+      before do
+        @server = Socket.new(family, :DGRAM)
+        @client = Socket.new(family, :DGRAM)
 
-      @server.bind(Socket.sockaddr_in(0, '127.0.0.1'))
+        @server.bind(Socket.sockaddr_in(0, ip_address))
+      end
+
+      it 'raises Errno::EOPNOTSUPP' do
+        proc { @server.listen(1) }.should raise_error(Errno::EOPNOTSUPP)
+      end
     end
 
-    it 'raises Errno::EOPNOTSUPP' do
-      proc { @server.listen(1) }.should raise_error(Errno::EOPNOTSUPP)
-    end
-  end
+    describe 'using a STREAM socket' do
+      before do
+        @server = Socket.new(family, :STREAM)
+        @client = Socket.new(family, :STREAM)
 
-  describe 'using a STREAM socket' do
-    before do
-      @server = Socket.new(:INET, :STREAM)
-      @client = Socket.new(:INET, :STREAM)
+        @server.bind(Socket.sockaddr_in(0, ip_address))
+      end
 
-      @server.bind(Socket.sockaddr_in(0, '127.0.0.1'))
-    end
+      it 'returns 0' do
+        @server.listen(1).should == 0
+      end
 
-    it 'returns 0' do
-      @server.listen(1).should == 0
-    end
-
-    it "raises when the given argument can't be coerced to a Fixnum" do
-      proc { @server.listen('cats') }.should raise_error(TypeError)
+      it "raises when the given argument can't be coerced to a Fixnum" do
+        proc { @server.listen('cats') }.should raise_error(TypeError)
+      end
     end
   end
 end
