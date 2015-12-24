1c1d04c45593b9af17ac6d1294ae837cdefd01c2
Yorick Peterse <yorickpeterse@gmail.com>
2015-12-24 14:54:20 +0100

    Added better specs for Addrinfo#canonname

--- spec/addrinfo/canonname_spec.rb
+++ spec/addrinfo/canonname_spec.rb
@@ -2,18 +2,20 @@ require 'socket'
 
 describe 'Addrinfo#canonname' do
   describe 'when the canonical name is available' do
-    it 'returns the canonical name' do
-      # TODO: figure out how we're going to set the canonical name
+    it 'returns the canonical name as a String' do
+      args  = ['localhost', nil, :INET, :STREAM, nil, Socket::AI_CANONNAME]
+      canon = Socket.getaddrinfo(*args)[0][2]
+      addrs = Addrinfo.getaddrinfo(*args)
+
+      addrs[0].canonname.should == canon
     end
   end
 
   describe 'when the canonical name is not available' do
-    before do
-      @addr = Addrinfo.new('127.0.0.1', 9999)
-    end
-
     it 'returns nil' do
-      @addr.canonname.should be_nil
+      addr = Addrinfo.new(Socket.sockaddr_in(0, '127.0.0.1'))
+
+      addr.canonname.should be_nil
     end
   end
 end
