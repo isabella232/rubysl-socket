fb1945d8c9d93d692057c1c09633ec1eefee29f2
Yorick Peterse <yorickpeterse@gmail.com>
2015-12-24 16:27:09 +0100

    Cleaned up Addrinfo#connect_from specs a bit

--- spec/addrinfo/connect_from_spec.rb
+++ spec/addrinfo/connect_from_spec.rb
@@ -5,6 +5,7 @@ describe 'Addrinfo#connect_from' do
     before do
       @server = TCPServer.new(ip_address, 0)
       @port   = @server.connect_address.ip_port
+      @addr   = Addrinfo.tcp(ip_address, @port)
     end
 
     after do
@@ -13,26 +14,25 @@ describe 'Addrinfo#connect_from' do
 
     describe 'using separate arguments' do
       it 'returns a Socket when no block is given' do
-        socket = Addrinfo.tcp(ip_address, @port).connect_from(ip_address, 0)
+        socket = @addr.connect_from(ip_address, 0)
 
         socket.should be_an_instance_of(Socket)
       end
 
       it 'yields the Socket when a block is given' do
-        Addrinfo.tcp(ip_address, @port).connect_from(ip_address, 0) do |socket|
+        @addr.connect_from(ip_address, 0) do |socket|
           socket.should be_an_instance_of(Socket)
         end
       end
 
       it 'treats the last argument as a set of options if it is a Hash' do
-        socket = Addrinfo.tcp(ip_address, @port)
-          .connect_from(ip_address, 0, timeout: 2)
+        socket = @addr.connect_from(ip_address, 0, timeout: 2)
 
         socket.should be_an_instance_of(Socket)
       end
 
       it 'binds the socket to the local address' do
-        socket = Addrinfo.tcp(ip_address, @port).connect_from(ip_address, 0)
+        socket = @addr.connect_from(ip_address, 0)
 
         socket.local_address.ip_address.should == ip_address
 
@@ -43,30 +43,29 @@ describe 'Addrinfo#connect_from' do
 
     describe 'using an Addrinfo as the 1st argument' do
       before do
-        @addr = Addrinfo.tcp(ip_address, 0)
+        @from_addr = Addrinfo.tcp(ip_address, 0)
       end
 
       it 'returns a Socket when no block is given' do
-        socket = Addrinfo.tcp(ip_address, @port).connect_from(@addr)
+        socket = @addr.connect_from(@from_addr)
 
         socket.should be_an_instance_of(Socket)
       end
 
       it 'yields the Socket when a block is given' do
-        Addrinfo.tcp(ip_address, @port).connect_from(@addr) do |socket|
+        @addr.connect_from(@from_addr) do |socket|
           socket.should be_an_instance_of(Socket)
         end
       end
 
       it 'treats the last argument as a set of options if it is a Hash' do
-        socket = Addrinfo.tcp(ip_address, @port)
-          .connect_from(@addr, timeout: 2)
+        socket = @addr.connect_from(@from_addr, timeout: 2)
 
         socket.should be_an_instance_of(Socket)
       end
 
       it 'binds the socket to the local address' do
-        socket = Addrinfo.tcp(ip_address, @port).connect_from(@addr)
+        socket = @addr.connect_from(@from_addr)
 
         socket.local_address.ip_address.should == ip_address
 
