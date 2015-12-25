4959ca480f2b9dc5a66fa9e40793b57d97755e85
Yorick Peterse <yorickpeterse@gmail.com>
2015-12-25 01:05:41 +0100

    Added specs for Addrinfo#marshal_dump

--- spec/addrinfo/marshal_dump_spec.rb
+++ spec/addrinfo/marshal_dump_spec.rb
@@ -1,5 +1,42 @@
 require 'socket'
 
-describe "Addrinfo#marshal_dump" do
-  it "needs to be reviewed for spec completeness"
+describe 'Addrinfo#marshal_dump' do
+  before do
+    @addr = Addrinfo.getaddrinfo('localhost', 80, :INET, :STREAM,
+                                 Socket::IPPROTO_TCP, Socket::AI_CANONNAME)[0]
+  end
+
+  it 'returns an Array' do
+    @addr.marshal_dump.should be_an_instance_of(Array)
+  end
+
+  describe 'the returned Array' do
+    before do
+      @array = @addr.marshal_dump
+    end
+
+    it 'includes the address family as the 1st value' do
+      @array[0].should == 'AF_INET'
+    end
+
+    it 'includes the IP address as the 2nd value' do
+      @array[1].should == [@addr.ip_address, @addr.ip_port.to_s]
+    end
+
+    it 'includes the protocol family as the 3rd value' do
+      @array[2].should == 'PF_INET'
+    end
+
+    it 'includes the socket type as the 4th value' do
+      @array[3].should == 'SOCK_STREAM'
+    end
+
+    it 'includes the protocol as the 5th value' do
+      @array[4].should == 'IPPROTO_TCP'
+    end
+
+    it 'includes the canonical name as the 6th value' do
+      @array[5].should == @addr.canonname
+    end
+  end
 end
